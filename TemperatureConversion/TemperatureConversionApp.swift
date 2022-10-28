8c4ff631cc56e40120583a569c68a55243e446ba
Cesar Ibarra <ibarratcesar@icloud.com>
2022-10-28 11:34:35 -0700

    Initial Commit

---
+++ TemperatureConversion/TemperatureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionApp.swift
+//  TemperatureConversion
+//
+//  Created by Cesar Ibarra on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
