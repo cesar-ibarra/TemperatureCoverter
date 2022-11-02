8fcc97297ed043aee51fca93a214835e75637a05
Cesar Ibarra <ibarratcesar@icloud.com>
2022-11-02 11:09:44 -0700

    change temperature calculator function using UnitTemperature

--- TemperatureConversion/ContentView.swift
+++ TemperatureConversion/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var initialValue: Double = 0
+    @State private var inputUnit = "Celsius"
+    @State private var outputUnit = "Celsius"
+    
+    let units = ["Celcius": UnitTemperature.celsius, "Fahrenheit": UnitTemperature.fahrenheit, "Kelvin": UnitTemperature.kelvin]
+    
+    var result: Double {
+        guard
+            let inputUnit = units[inputUnit],
+            let outputUnit = units[outputUnit]
+        else { return 0 }
+        
+        let input = Measurement(value: initialValue, unit: inputUnit)
+        let output = input.converted(to: outputUnit)
+        
+        return output.value
+    }
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Convert")) {
+                        TextField("Temperature", value: $initialValue, format: .number)
+                            .keyboardType(.decimalPad)
+                            .font(.system(size: 40))
+                        Picker("Temperature", selection: $inputUnit) {
+                            ForEach(Array(units.keys), id: \.self) {
+                                Text($0)
+                            }//: FOREACH
+                        } //: PICKER
+                        .pickerStyle(.segmented)
+                    } //: SECTION
+                    
+                    Section(header: Text("To:")) {
+                        Picker("Value", selection: $outputUnit) {
+                            ForEach(Array(units.keys), id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+                    Section(header: Text("Result")) {
+                        Text("\(result, specifier: "%.2f")")
+                            .font(.system(size: 40))
+                    } //: SECTION
+                } //: FORM
+            } //: VSTACK
+        } //: NavigationView
     }
 }
 
