8a8ab7d3e3e0c3b102b2aa02528266622ba5b4aa
Cesar Ibarra <ibarratcesar@icloud.com>
2025-12-15 20:13:33 -0800

    You can enter values by pressing in the center of the circle

---
+++ TemperatureConverter/AdMobBanner.swift
@@ -0,0 +1,8 @@
+//
+//  AdMobBanner.swift
+//  TemperatureConverter
+//
+//  Created by Cesar Ibarra on 3/4/25.
+//
+
+import Foundation
---
+++ TemperatureConverter/Views/CircularSlider.swift
@@ -0,0 +1,18 @@
+//
+//  CircularSlider.swift
+//  TemperatureConverter
+//
+//  Created by Cesar Ibarra on 2/28/25.
+//
+
+import SwiftUI
+
+struct CircularSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularSlider()
+}
